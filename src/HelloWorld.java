29759066c54d4e1ea4915309364f1c973311845f
USER <USER@SAMSUNG>
2020-03-24 11:30:59 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("***BOOK MANAGEMENT PROGRAM***");
+		System.out.println("1. Borrow Books");
+		System.out.println("2. Return Books");
+		System.out.println("3. Extension of The Borrow Period");
+		System.out.println("4. Book Search");
+		System.out.println("Please select one number between 1 - 5");
+	}
+
+}
