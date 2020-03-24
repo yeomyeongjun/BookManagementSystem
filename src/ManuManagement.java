d5e5067cc25bd9de6bbd489fbf95503c53661a40
USER <USER@SAMSUNG>
2020-03-24 12:21:42 +0900

    correct errors

--- src/ManuManagement.java
+++ src/ManuManagement.java
@@ -1,12 +1,17 @@
-public class HelloWorld {
+public class ManuManagement {
 
 	public static void main(String[] args) {
+		System.out.println("========================================");
 		System.out.println("***BOOK MANAGEMENT PROGRAM***");
-		System.out.println("1. Borrow Books");
-		System.out.println("2. Return Books");
-		System.out.println("3. Extension of The Borrow Period");
-		System.out.println("4. Book Search");
+		System.out.println("----------------------------------------");
+		System.out.println("1. Borrow Books"); // 도서 대출
+		System.out.println("2. Return Books"); // 도서 반납
+		System.out.println("3. Extension of The Borrow Period"); // 대출 기간 연장
+		System.out.println("4. Book Search"); // 도서 검색
+		System.out.println("5. Exit"); // 나가기
+		System.out.println("----------------------------------------");
 		System.out.println("Please select one number between 1 - 5");
+		System.out.println("========================================");
 	}
 
 }
