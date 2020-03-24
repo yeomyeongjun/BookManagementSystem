33dc0e423e59b2a19c86cdaca2472a327eef911e
yeomyeongjun <62334607+yeomyeongjun@users.noreply.github.com>
2020-03-24 10:38:06 +0900

    Update and rename Hello.java to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,10 @@
+package helloworld;
 
-public class Hello {
+public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("***Book Management Program***");
-		
+
 	}
 
 }
