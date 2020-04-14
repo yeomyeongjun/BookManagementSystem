05fd10e163be340885db3645354e5d92aaa15bff
USER <USER@SAMSUNG>
2020-04-14 18:14:29 +0900

    Edit two menu: extend the borrow period(3) -> edit, search(4) -> view
    using an array to store data

--- src/ManuManagement.java
+++ src/ManuManagement.java
@@ -13,8 +13,8 @@ public class ManuManagement {
 			System.out.println("----------------------------------------");
 			System.out.println("1. Borrow Books"); // 도서 대출
 			System.out.println("2. Return Books"); // 도서 반납
-			System.out.println("3. Extension of The Borrow Period"); // 대출 기간 연장
-			System.out.println("4. Book Search"); // 도서 검색
+			System.out.println("3. Edit Book Information"); // 도서 정보 수정
+			System.out.println("4. View All Books"); // 도서 검색
 			System.out.println("5. Exit"); // 나가기
 			System.out.println("----------------------------------------");
 			System.out.println("Please select one number between 1 - 5");
@@ -27,9 +27,9 @@ public class ManuManagement {
 			else if (num == 2)
 				bookManager.returnBook();
 			else if (num == 3)
-				bookManager.extend();
+				bookManager.edit();
 			else if (num == 4)
-				bookManager.search();
+				bookManager.view();
 			else if (num == 5)
 				break;
 			else
